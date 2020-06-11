afc775e4daebe220645f9bb4610b53027f975616
unknown <kevhemsoth@gmail.com>
2020-06-10 21:38:29 -0400

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const Sequelize = require('sequelize');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const PORT = 3000 || process.env.PORT;
+
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors());
+
+
+const sequelize = new Sequelize('track_notes', process.env.DB_USER, process.env.DB_PASS, {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+
+
+
+app.listen(PORT, () =>{
+  console.log(`App listening on port ${PORT}`);
+}
+)
